15623ca934076f2d549f073bc23c788d2e962a65
Katniss-Nataliia <natko.boiko@gmail.com>
2023-06-08 17:37:13 -0600

    changes

--- js/02-timer.js
+++ js/02-timer.js
@@ -1,8 +1,64 @@
-const calendar = document.querySelector("#datetime-picker");
-const startBttn = document.querySelector("[data-start]");
-const timer = document.querySelector(".timer")
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css';
+import {Notify} from 'notiflix/build/notiflix-aio';
 
-startBttn.disabled = true;
+const calendar = document.querySelector("#datetime-picker");
+const startBttn = document.querySelector("button[data-start]");
+const timer = document.querySelector(".timer");
+const daysEl = document.querySelector("span[data-days]");
+const hoursEl = document.querySelector("span[data-hours]");
+const minutesEl = document.querySelector("span[data-minutes]");
+const secondsEl = document.querySelector("span[data-seconds]");
+
+let userDate  = null;
+let isActive = false;
+let timerId = null;
+
+function pad (value){
+    return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+}
+function startCountDown(){
+    calendar.disabled=true;
+    startBttn.disabled = true;
+    if(isActive){
+        return;
+    }
+    isActive = true;
+    timerId = setInterval(()=>{
+        const currentTime = Date.now();
+        const distance = userDate - currentTime;
+        const components = convertMs(distance);
+
+        daysEl.textContent = components.days;
+        hoursEl.textContent = components.hours;
+        minutesEl.textContent = components.minutes;
+        secondsEl.textContent = components.seconds;
+
+        if (distance <= 0){
+            clearInterval(timerId);
+            timer.innerHTML = 'Time is over!'
+        }
+    }, 1000)
+}
 
 const options = {
     enableTime: true,
@@ -10,27 +66,19 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        if(selectedDates < this.defaultDate){
-            window.alert("Please choose a date in the future")
+        if(selectedDates[0] < Date.now()){
+           Notify.failure ("Please choose a date in the future");
+           userDate = new Date();
         }
-
-        // if statement not working 
-
-        startBttn.disabled = false;
-        console.log(selectedDates[0]);
+        else{
+            startBttn.disabled = false;
+            userDate = selectedDates[0]
+        }
+        
         
     },
   };
 
 flatpickr(calendar, options);
-
-
 startBttn.addEventListener("click", startCountDown);
 
-function startCountDown(){
-    const currentTime = new Date();
-    const now = currentTime.getTime();
-    const distance = selectedDates - now;
-    console.log(distance)
-    
-}
